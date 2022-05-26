eaf52c067050a48f11782572f509bd6861c95963
neel123go <ayounpaul1029@gmail.com>
2022-05-26 22:02:41 +0600

    edit footer section

--- src/Components/Pages/Shared/Footer.js
+++ src/Components/Pages/Shared/Footer.js
@@ -13,18 +13,16 @@ const Footer = () => {
                     <p className='text-xl'>Mr Computer Tools</p>
                 </div>
                 <div>
-                    <span className='text-lg font-semibold'>Services</span>
-                    <Link to='/' className="link link-hover">Branding</Link>
-                    <Link to='/' className="link link-hover">Design</Link>
-                    <Link to='/' className="link link-hover">Marketing</Link>
-                    <Link to='/' className="link link-hover">Advertisement</Link>
+                    <span className='text-lg font-semibold'>Pages</span>
+                    <Link to='/myPortfolio' className="link link-hover">My Portfolio</Link>
+                    <Link to='/blogs' className="link link-hover">Blogs</Link>
+                    <Link to='/dashboard' className="link link-hover">Dashboard</Link>
                 </div>
                 <div>
-                    <span className="text-lg font-semibold">Legal</span>
-                    <Link to='/' className="link link-hover">About us</Link>
-                    <Link to='/' className="link link-hover">Contact</Link>
-                    <Link to='/' className="link link-hover">Jobs</Link>
-                    <Link to='/' className="link link-hover">Press kit</Link>
+                    <span className="text-lg font-semibold">Office</span>
+                    <p>Meghna C-31, Dariapara, Sylhet</p>
+                    <p>phone - 01726384739</p>
+                    <p>email - mr.conputer@gmail.com</p>
                 </div>
             </footer>
             <footer className="footer px-8 lg:px-32 text-center flex justify-center items-center py-4 border-t bg-base-300 border-gray-400">
